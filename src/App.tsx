282bf0f77f40c2b247fb1d7d0d3d7776b8bd9ec1
richardpiuschua <piuslchua@gmail.com>
2026-01-21 11:12:49 +0530

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
       const data = await res.json();
       setMessage(JSON.stringify(data, null, 2)); // nicely formatted
     } catch (err: any) {
-      setMessage("Error: " + err.message);
+      setMessage("Errsor: " + err.message);
     }
   };
 
