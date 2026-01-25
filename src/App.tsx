d6e30b3b2cb0cfa0a5a3ee895a0f20c126a5d018
richardpiuschua <piuslchua@gmail.com>
2026-01-26 00:52:27 +0530

    d

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <>
-      <h1>Vite + Reasactaaaa</h1>
+      <h1>Vite + Reasactaaasa</h1>
 
       <div className="card">
         <button onClick={() => setCount((c) => c + 1)}>
